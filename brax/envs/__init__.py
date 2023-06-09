89beb2678c964c3d0f150b4ff1b76953d9230bec
Joaquin Arias <64325659+jariasn@users.noreply.github.com>
2023-06-09 09:45:52 +0200

    Update __init__.py

--- brax/envs/__init__.py
+++ brax/envs/__init__.py
@@ -29,6 +29,7 @@ from brax.envs import inverted_pendulum
 from brax.envs import pusher
 from brax.envs import reacher
 from brax.envs import walker2d
+from brax.envs import ur5
 from brax.envs.base import Env, PipelineEnv, State, Wrapper
 from brax.envs.wrappers import training
 
@@ -44,6 +45,7 @@ _envs = {
     'pusher': pusher.Pusher,
     'reacher': reacher.Reacher,
     'walker2d': walker2d.Walker2d,
+    'ur5': ur5.UR5,
 }
 
 
